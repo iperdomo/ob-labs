cc93ee4280ab7bb3fd39a9722cb164de3a782c6a
Iv?n Perdomo <ivan.perdomo@openbravo.com>
2009-09-25 13:02:13 +0200

    Added useful-queries.sql

---
+++ openbravo/useful-queries.sql
@@ -0,0 +1,8 @@
+-- Application Dictionary, table & column definition
+select t.tablename, c.name, c.columnname, r.name as referencename, c.ad_reference_value_id,
+       c.iskey, c.isparent, c.isidentifier, c.issecondarykey
+  from ad_table t, ad_column c, ad_reference r
+ where t.ad_table_id = c.ad_table_id
+   and upper(t.tablename) like '%C_BPARTNER%' -- Change this with your table name in UPPERCASE
+   and c.ad_reference_id = r.ad_reference_id
+  order by c.created;

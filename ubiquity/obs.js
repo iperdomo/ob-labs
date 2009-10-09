74df765b742bfe8472c416fcbf6cb18dc511db94
Iván Perdomo <iperdomo@users.noreply.github.com>
2009-10-09 18:08:01 +0200

    Added Openbravo wiki search

---
+++ ubiquity/obs.js
@@ -0,0 +1,18 @@
+CmdUtils.CreateCommand({
+  names: ["obs"],
+  description: "Ubiquity command for search in wiki.openbravo.com.",
+  help: "s <b>some seach string</b>.",
+  author: {name: "Iván Perdomo", email: "ivan.perdomo@openbravo.com"},
+  license: "GPL",
+  homepage: "http://katratxo.wordpress.com/",
+  arguments: [{role: 'object', nountype: noun_arb_text}],
+  preview: function preview(pblock, args) {
+    pblock.innerHTML = "Your input is <b>" + args.object.html + "</b>.";
+  },
+  execute: function execute(args) {
+  var query = encodeURIComponent(args.object.text);
+    if(query != "") {
+      Utils.openUrlInBrowser("http://www.google.com/search?hl=en&q="+query+"+site%3Awiki.openbravo.com")
+    }
+  }
+});
